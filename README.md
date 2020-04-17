4580ab8d0317131ccc5a2d69c1b24306e16d80d9
kisakyamukama <you@efitzerkisakyamukama03@gmail.com>
2020-04-17 16:37:46 +0300

    update leassons learnt

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # dictionary_app
+## Concept
+- Getting api data and diplaying it on the screen.
+- Using Stream builder.
+
+## Streambuilder 
+- this was thenew key concept I learnt in this project. 
+- Stream builder rebuilds widget whenever it gets data 
+
+## Appbar
+- bottom feature of the appbar that enables us to add widgets and in this project we used it to add a search input .
+
+## Debounce 
+Helps us to time events, in this project it is used to execute a search if a user poses for one second after typing in some characters, so u may not need to tap search.
+
+I have liked this project, it is simple but cool!
 
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,26 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'M dictionary',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'M Dictionary'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -44,68 +28,120 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _url = 'https://owlbot.info/api/v4/dictionary/';
+  String _token = "33fb83fb78b95813ef8349135167143f4a56dc9f";
+  TextEditingController _controller = TextEditingController();
+  StreamController _streamController;
+  Stream _stream;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Timer _debounce;
+
+  _search() async { 
+    if(_controller.text == null || _controller.text.length == 0) {
+      _streamController.add(null);
+      return;
+    }
+    _streamController.add("waiting");
+    Response response = await  get(_url + _controller.text, headers: { "Authorization": "Token " + _token });
+    print('Response ${response.body}');
+    _streamController.add(json.decode(response.body));
+   
+  }
+
+  @override 
+  void initState() {
+    super.initState();
+    _streamController = StreamController();
+    _stream = _streamController.stream;
+
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          title: Text(widget.title),
+          centerTitle: true,
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(48.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 12.0, bottom: 8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(24.0),
+                    ),
+                    child: TextFormField(
+                      onChanged: (String text) {
+                        if(_debounce?.isActive ?? false) _debounce.cancel(); // check if active then cancel
+                        _debounce = Timer(const Duration(milliseconds: 1000), () { // if user does not place any key after a second call
+                          _search();
+                        });
+                      },
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        hintText: 'Search for a word',
+                        contentPadding: const EdgeInsets.only(left: 24.0),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    _search();
+                  },
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+          )),
+      body: Container(
+         margin:const EdgeInsets.all(8.0) ,
+        child: StreamBuilder( 
+          stream: _stream,
+          builder: (BuildContext cxt, AsyncSnapshot snapshot ) {
+            if (snapshot.data == 'waiting') {
+              return Center( 
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.data  == null ) {
+              return Center( 
+                child: Text('Enter a search word'),
+              );
+            }
+             return ListView.builder(
+              itemCount: snapshot.data['definitions'].length,
+              itemBuilder: (BuildContext context, int index) {
+              return ListBody(children: <Widget>[
+                Container( 
+                  color: Colors.grey[300],
+                  child: ListTile( 
+                    leading: snapshot.data['definitions'][index]['image_url'] == null ? null : CircleAvatar( 
+                      backgroundImage: NetworkImage(snapshot.data['definitions'][index]['image_url']),
+                    ),
+                    title: Text(_controller.text.trim() + "(" + snapshot.data["definitions"][index]['type'] + ")"),
+                  ),
+                  
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(snapshot.data['definitions'][index]['definition']),
+                )
+              ],);
+           },
+          );
+          },
+         
+
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
